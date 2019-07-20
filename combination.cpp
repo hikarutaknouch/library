45cb3605fcc138108aab3002d134cfba24231433
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-07-20 11:36:46 +0900

    add combination

---
+++ combination.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main() {
+    // 前処理
+    COMinit();
+
+    // 計算例
+    cout << COM(100000, 50000) << endl;
+}
