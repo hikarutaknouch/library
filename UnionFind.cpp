36e45d561d0e22b5c5b66781fe17daae287bf1e0
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-08-12 17:08:04 +0900

    unionFind

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -12,32 +12,31 @@ const ll MOD = 1000000007;
 const ll INF = 1000000000;
 
 struct UnionFind {
-    vector<int> data;
-    void init(int n) {
-        data.assign(n,-1);
+    vector<int> par, size;
+    UnionFind(int n) : par(n), size(n) {
+        for(int i = 0; i < n; ++i) par[i] = i;
+        size.assign(n,1);
     }
     void Union(int x, int y) {
         x = Find(x), y = Find(y);
         if(x == y) return;
-        if(data[x] > data[y]) swap(x, y);
-        data[x] += data[y];
-        data[y] = x;
+        if(size[x] < size[y]) swap(x,y);
+        size[x] += size[y];
+        par[y] = x;
     }
     int Find(int x) {
-        if(data[x]<0) return x;
-        else return data[x] = Find(data[x]);
+        if(par[x]==x) return x;
+        else return par[x] = Find(par[x]);
     }
-    bool same(int x, int y) {
+    bool Same(int x, int y) {
         return Find(x) == Find(y);
-    }
-    int size(int x) {
-        return -data[Find(x)];
     }
 };
 
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    
+    int n;
+    UnionFind uf(n);
     return 0;
 }
