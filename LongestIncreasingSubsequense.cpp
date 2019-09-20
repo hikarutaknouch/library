d20f8bd712108037bd0f1527bb5063e327183d0a
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-09-21 01:15:59 +0900

    empire

--- LongestIncreasingSubsequense.cpp
+++ LongestIncreasingSubsequense.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#define REPP(i, n) for(int i = 1; i <= n; ++i)
-#define ALL(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
 
@@ -8,7 +6,7 @@ int lis(int n, vector<int>& v) {
     vector<int> dp(n+1, 0);
     dp[0] = v[0];
     int length = 1;
-    REPP(i,n-1) {
+    for(int i = 1; i <= n-1; ++ i) {
         if(dp[length-1]<v[i]) dp[length++] = v[i];
         else *lower_bound(dp.begin(), dp.begin()+length, v[i]) = v[i];
     }
--- gcd.cpp
+++ gcd.cpp
@@ -4,12 +4,9 @@
 #define REPR(i, n) for(int i = n; i >= 0; --i)
 #define FOR(i, m, n) for(int i = m; i < n; ++i)
 #define ALL(v) v.begin(), v.end()
-#define UNIQUE(v) v.erase(unique(v.begin(), v.end()), v.end());
 using namespace std;
 typedef long long ll;
-typedef pair<ll, ll> P;
 const ll MOD = 1000000007;
-const ll INF = 1000000000;
 
 ll gcd(ll x, ll y) {
     if(x<y) gcd(y,x);
